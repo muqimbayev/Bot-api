4adad11496e40756083639fda5f1cc287482e1a2
muqimbayev <muqimbayev@gmail.com>
2024-03-08 09:37:40 +0500

    first

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ["*"]
 
 
@@ -163,3 +162,6 @@ SWAGGER_SETTINGS = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
